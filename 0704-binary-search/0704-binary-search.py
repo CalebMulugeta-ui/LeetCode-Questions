d789550f11906876e19d584d2b6d75757ea31869
Caleb Mulugeta <calebmulugeta75@gmail.com>
2025-12-06 20:31:44 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+
+        while starts <= end:
+            
+            mid = (end+start) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+                
+        return -1
+
+        
