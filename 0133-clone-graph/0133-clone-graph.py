d3e01dd3b09d1cead21a84f56f11be2646abe894
Caleb Mulugeta <calebmulugeta75@gmail.com>
2026-02-09 14:55:27 -0500

    Time: 42 ms (12.42%), Space: 12.8 MB (33.79%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque 
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+
+
+        oldToNew = {node: Node(node.val)}
+        queue = deque([node])
+
+        while queue:
+            curr = queue.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in oldToNew:
+                    oldToNew[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                
+                oldToNew[curr].neighbors.append(oldToNew[neighbor])
+                
+        return oldToNew[node]
